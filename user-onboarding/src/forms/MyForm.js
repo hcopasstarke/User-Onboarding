ad03cd6054595d3bdb25bde7063ce9df149c67c0
hcopasstarke <hcopasstarke@gmail.com>
2019-07-18 11:51:52 -0700

    MVP

--- user-onboarding/src/forms/MyForm.js
+++ user-onboarding/src/forms/MyForm.js
@@ -2,71 +2,113 @@ import React from 'react';
 import { withFormik, Form, Field } from 'formik';
 import axios from 'axios';
 import * as Yup from 'yup';
+import './MyForm.css';
 
 function MyForm({ values, errors, touched, isSubmitting }) {
     console.log(isSubmitting);
     return(
         <Form>
-            <div>
-                <label>Name</label>
+            <div className="login-group">
+                <label>Name</label> 
                 {touched.name && errors.name && <p>{errors.name}</p>}
-                <Field type='text' name='name' placeholder='Name' />
+                <Field 
+                    type='text' 
+                    name='name' 
+                    placeholder='Name' 
+                    value={values.name}
+                />
             </div>
-            <div>
+            <div className="login-group">
                 <label>Email</label>
                 {touched.email && errors.email && <p>{errors.email}</p>}
-                <Field type='email' name='email' placeholder='Email' />
+                <Field 
+                    type='email' 
+                    name='email' 
+                    placeholder='Email' 
+                    value={values.email}
+                />
             </div>
-            <div>
+            <div className="login-group">
                 <label>Password</label>
                 {touched.password && errors.password && <p>{errors.password}</p>}
-                <Field type='password' name='password' placeholder='Password' />
+                <Field
+                    autoComplete='off'
+                    type='password' 
+                    name='password' 
+                    placeholder='Password' 
+                    value={values.password}
+                />
             </div>
-            <div>
+            <div className='login-group'>
+                <label>User Type</label>
+                <Field 
+                    component='select' 
+                    name='role'
+                >
+                    <option value='Editor'>Editor</option>
+                    <option value='Reviewer'>Reviewer</option>
+                    <option value='Admin'>Admin</option>
+                </Field>
+            </div>
+            <div className='login-group-2'>
                 <label>Terms of Service</label>
-                <Field type='checkbox' name='terms' placeholder='Terms of Service' />
+                {isSubmitting && <p>Loading...</p>}
+                <Field 
+                    type='checkbox'
+                    name='terms'
+                    id='terms' 
+                    checked={values.terms} 
+                />
             </div>
-            <button>Submit</button>
+            <div className='login-group'>
+                <button
+                    disabled={isSubmitting}
+                    type='submit'
+                >
+                    Submit    
+                </button>
+            </div>
         </Form>
     );
 }
 
 export default withFormik({
-    mapPropsToValue: () => {
+    mapPropsToValue: ({name, password, email, terms, role}) => {
         return {
-            name: '',
-            email: '',
-            password: '',
-            checkbox: ''
+            name: name || '',
+            email: email || '',
+            password: password || '',
+            terms: terms,
+            role: role || 'Editor'
         };
-    },
-
-    handleSubmit: (values) => {
-        console.log(values);
-        const url = 'https://reqres.in/api/users';
-
-        axios
-        .post(url, values)
-        .then(response => {
-            console.log(response.data);
-        })
-        .catch(error => {
-            console.log(error);
-        });
     },
 
     validationSchema: Yup.object().shape({
         name: Yup.string()
-            .min(3, 'Name must be at least 3 characters long')
-            .max(12, 'Name must be less than 12 characters')
-            .required('Name is required'),
+            .required('Name is required.'),
         password: Yup.string()
-            .min(8)
+            .min(8, 'Passwords must be 8-18 characters.')
             .max(18)
-            .required('Password is required'),
+            .required('Password is required.'),
         email: Yup.string()
-            .min(10)
-            .max(25)
-            .required('Email is required')
-    })
+            .email()
+            .required('Email is required.')
+    }),
+
+    handleSubmit(values, {resetForm, setErrors, setSubmitting}) {
+        if (values.email === 'dummy@gmail.com') {
+            setErrors({email: 'Email is already taken.'});
+        } else {
+            axios
+                .post('https://reqres.in/api/users', values)
+                .then(response => {
+                console.log('URL', response);
+                alert('Your registered email is: ' + response.data.email);
+                resetForm();
+                setSubmitting(false);
+                })
+                .catch(error => console.log(error))
+        }
+    }
+
 })(MyForm);
